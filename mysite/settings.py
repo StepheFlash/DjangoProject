9d41a5aa45a207601a81a45839dda289d9455d40
StepheFlash <david.palacios.forero@gmail.com>
2026-01-22 12:42:28 -0500

    Error loops HTTPS

--- mysite/settings.py
+++ mysite/settings.py
@@ -179,13 +179,23 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Security settings for production
 if not DEBUG:
+    # Trust X-Forwarded-Proto header from Railway proxy
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     SECURE_SSL_REDIRECT = True
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
     SECURE_BROWSER_XSS_FILTER = True
+    SECURE_HSTS_SECONDS = 31536000  # 1 year
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
     SECURE_CONTENT_SECURITY_POLICY = {
         'default-src': ("'self'",),
         'script-src': ("'self'", "'unsafe-inline'", "'unsafe-eval'"),
         'style-src': ("'self'", "'unsafe-inline'"),
         'img-src': ("'self'", "data:", "https:"),
     }
+else:
+    # Development: disable redirect and secure cookies
+    SECURE_SSL_REDIRECT = False
+    SESSION_COOKIE_SECURE = False
+    CSRF_COOKIE_SECURE = False
